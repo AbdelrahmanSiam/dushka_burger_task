2b9434e65220900f688da480d9fa6f5ed8ecd9be
Abdulrahman <syamabdo382@gmail.com>
2026-03-09 01:42:17 +0200

    add localizationsDelegates and supportedLocales to material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:dusks_burger_task/core/utils/app_router.dart';
 import 'package:dusks_burger_task/features/languages/presentation/views/language_view.dart';
+import 'package:dusks_burger_task/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,12 +15,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       routerConfig: AppRouter.router,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xFFFAF3E9),
-      ),
+      theme: ThemeData(scaffoldBackgroundColor: Color(0xFFFAF3E9)),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-

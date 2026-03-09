d59d8d068f18058479277db4bd1e61c7ff463a9a
Abdulrahman <syamabdo382@gmail.com>
2026-03-09 02:03:35 +0200

    add flutter_bloc package

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:dusks_burger_task/features/categories/presentation/views/categories_view.dart';
 import 'package:dusks_burger_task/features/languages/presentation/views/language_view.dart';
 import 'package:go_router/go_router.dart';
 
@@ -8,6 +9,7 @@ class AppRouter {
     initialLocation: languageView,
     routes: [
       GoRoute(path: languageView, builder: (_, __) => const LanguageView()),
+      GoRoute(path: categoriesView, builder: (_, __) => const CategoriesView()),
     ],
   );
 }

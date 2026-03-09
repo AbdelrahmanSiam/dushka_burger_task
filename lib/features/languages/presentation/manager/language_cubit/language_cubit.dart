360f0c3441a2cebc03225103bf97359fa6c1032f
Abdulrahman <syamabdo382@gmail.com>
2026-03-09 03:18:17 +0200

    Refactor cubit to use Usecases

--- lib/features/languages/presentation/manager/language_cubit/language_cubit.dart
+++ lib/features/languages/presentation/manager/language_cubit/language_cubit.dart
@@ -1,11 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:dusks_burger_task/features/languages/domain/use_cases/change_language_usecase.dart';
+import 'package:dusks_burger_task/features/languages/domain/use_cases/get_saved_language_usecase.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 class LanguageCubit extends Cubit<Locale> {
-  LanguageCubit() : super(Locale("ar"));
+  final ChangeLanguageUsecase changeLanguageUseCase;
+  final GetSavedLanguageUseCase getSavedLanguageUseCase;
 
-  void changeLanguage(String languageCode) {
-    emit(Locale(languageCode));
+  LanguageCubit(this.changeLanguageUseCase, this.getSavedLanguageUseCase)
+    : super(Locale("ar"));
+
+  void changeLanguage(String code) async {
+    await changeLanguageUseCase(code);
+
+    emit(Locale(code));
+  }
+
+  void loadSavedLanguage() {
+    final code = getSavedLanguageUseCase();
+
+    emit(Locale(code));
   }
 }

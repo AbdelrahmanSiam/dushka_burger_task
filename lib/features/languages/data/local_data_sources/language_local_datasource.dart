1856ddd19d4384e5dca8988473015e2ecaf453ae
Abdulrahman <syamabdo382@gmail.com>
2026-03-09 03:16:01 +0200

    LanguageRepoImpl

--- lib/features/languages/data/local_data_sources/language_local_datasource.dart
+++ lib/features/languages/data/local_data_sources/language_local_datasource.dart
@@ -1,3 +1,16 @@
-class LanguageLocalDatasource {
-  
+import 'package:hive/hive.dart';
+
+class LanguageLocalDataSource {
+
+  final Box box;
+
+  LanguageLocalDataSource(this.box);
+
+  Future<void> saveLanguage(String code) async {
+    await box.put("language", code);
+  }
+
+  String getSavedLanguage() {
+    return box.get("language", defaultValue: "en");
+  }
 }
---
+++ lib/features/languages/data/repo/language_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:dusks_burger_task/features/languages/data/local_data_sources/language_local_datasource.dart';
+import 'package:dusks_burger_task/features/languages/domain/repo/language_repo.dart';
+
+class LanguageRepoImpl implements LanguageRepo{
+  final LanguageLocalDataSource languageLocalDataSource;
+
+  LanguageRepoImpl({required this.languageLocalDataSource});
+  @override
+  String getSavedLanguage() {
+    return languageLocalDataSource.getSavedLanguage();
+  }
+
+  @override
+  Future<void> saveLanguage(String code) {
+    return languageLocalDataSource.saveLanguage(code);
+  }
+
+}

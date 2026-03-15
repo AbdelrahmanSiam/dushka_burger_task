187b31f91e19f110e543e3688329f5396d204527
Abdulrahman <syamabdo382@gmail.com>
2026-03-15 06:17:30 +0200

    create BottomNavigationBarItems widget

--- lib/features/categories/presentation/views/widgets/bottom_navigation_bar.dart
+++ lib/features/categories/presentation/views/widgets/bottom_navigation_bar.dart
@@ -1,8 +1,6 @@
 import 'package:dusks_burger_task/core/utils/app_colors.dart';
-import 'package:dusks_burger_task/features/categories/presentation/views/widgets/navigation_item.dart';
-import 'package:dusks_burger_task/generated/l10n.dart';
+import 'package:dusks_burger_task/features/categories/presentation/views/widgets/bottom_navigation_bar_items.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   const CustomBottomNavigationBar({super.key});
@@ -22,36 +20,7 @@ class CustomBottomNavigationBar extends StatelessWidget {
           ),
         ],
       ),
-      child: Row(
-        children: [
-          NavigationItem(
-            outlinedIcon: Iconsax.home_2_copy,
-            selectedIcon: Iconsax.home_2,
-            iconName: S.of(context).home,
-          ),
-          Spacer(),
-          NavigationItem(
-            outlinedIcon: Iconsax.menu_board_copy,
-            selectedIcon: Iconsax.menu_board,
-            iconName: S.of(context).menu,
-            isSelected: true,
-          ),
-          Spacer(),
-          Icon(Iconsax.shopping_cart, size: 35,),
-          Spacer(),
-          NavigationItem(
-            outlinedIcon: Iconsax.percentage_circle_copy,
-            selectedIcon: Iconsax.percentage_circle,
-            iconName: S.of(context).offer,
-          ),
-          Spacer(),
-          NavigationItem(
-            outlinedIcon: Iconsax.user_copy,
-            selectedIcon: Iconsax.user,
-            iconName: S.of(context).person,
-          ),
-        ],
-      ),
+      child: BottomNavigationBarItems(),
     );
   }
 }
---
+++ lib/features/categories/presentation/views/widgets/bottom_navigation_bar_items.dart
@@ -0,0 +1,44 @@
+import 'package:dusks_burger_task/features/categories/presentation/views/widgets/navigation_item.dart';
+import 'package:dusks_burger_task/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+
+class BottomNavigationBarItems extends StatelessWidget {
+  const BottomNavigationBarItems({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        NavigationItem(
+          outlinedIcon: Iconsax.home_2_copy,
+          selectedIcon: Iconsax.home_2,
+          iconName: S.of(context).home,
+        ),
+        Spacer(),
+        NavigationItem(
+          outlinedIcon: Iconsax.menu_board_copy,
+          selectedIcon: Iconsax.menu_board,
+          iconName: S.of(context).menu,
+          isSelected: true,
+        ),
+        Spacer(),
+        Icon(Iconsax.shopping_cart_copy , size: 35,),
+        Spacer(),
+        NavigationItem(
+          outlinedIcon: Iconsax.percentage_circle_copy,
+          selectedIcon: Iconsax.percentage_circle,
+          iconName: S.of(context).offer,
+        ),
+        Spacer(),
+        NavigationItem(
+          outlinedIcon: Iconsax.user_copy,
+          selectedIcon: Iconsax.user,
+          iconName: S.of(context).person,
+        ),
+      ],
+    );
+  }
+}

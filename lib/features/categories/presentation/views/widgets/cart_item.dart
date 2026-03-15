cbd96d1304cd49d38edf4b692c9a89b9c38dcf55
Abdulrahman <syamabdo382@gmail.com>
2026-03-15 06:51:40 +0200

    Transform CartItem

--- lib/features/categories/presentation/views/widgets/cart_item.dart
+++ lib/features/categories/presentation/views/widgets/cart_item.dart
@@ -1,11 +1,22 @@
+import 'package:dusks_burger_task/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class CartItem extends StatelessWidget {
   const CartItem({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Icon(Iconsax.shopping_cart_copy, size: 30);
+    return Transform.translate(
+      offset: Offset(0, -MediaQuery.of(context).size.height * 0.035),
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          color: AppColors.navBarColor,
+          shape: BoxShape.circle,
+        ),
+        child: Icon(Iconsax.shopping_cart_copy, color: Colors.white, size: 30),
+      ),
+    );
   }
 }

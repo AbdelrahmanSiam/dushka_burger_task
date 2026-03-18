3996aaad5dc0837b1b81ef3cf739091e19f4bced
Abdulrahman <syamabdo382@gmail.com>
2026-03-18 13:42:07 +0200

    create ProductDescription

---
+++ lib/features/product_details/presentation/view/widgets/product_description.dart
@@ -0,0 +1,16 @@
+import 'package:dusks_burger_task/core/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class ProductDescription extends StatelessWidget {
+  const ProductDescription({
+    super.key, required this.productDescription,
+  });
+ final String productDescription;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      productDescription,
+      style: AppStyles.textStyleSemiBold15(context),
+    );
+  }
+}
--- lib/features/product_details/presentation/view/widgets/product_details_body.dart
+++ lib/features/product_details/presentation/view/widgets/product_details_body.dart
@@ -2,6 +2,7 @@ import 'package:dusks_burger_task/core/utils/app_assets.dart';
 import 'package:dusks_burger_task/core/utils/app_styles.dart';
 import 'package:dusks_burger_task/features/categories/presentation/views/models/product_model.dart';
 import 'package:dusks_burger_task/features/product_details/presentation/view/widgets/custom_divider.dart';
+import 'package:dusks_burger_task/features/product_details/presentation/view/widgets/product_description.dart';
 import 'package:dusks_burger_task/features/product_details/presentation/view/widgets/product_details_header.dart';
 import 'package:dusks_burger_task/features/product_details/presentation/view/widgets/product_details_section.dart';
 import 'package:flutter/material.dart';
@@ -28,10 +29,14 @@ class ProductDetailsBody extends StatelessWidget {
             ),
           ),
           CustomDivider(),
+          ProductDescription(
+            productDescription:
+                "الووووووووووووووووووووووووووووووووووووووووووووووووووووووصف",
+          ),
+          CustomDivider(),
+          
         ],
       ),
     );
   }
 }
-
-
